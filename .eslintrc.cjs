31c5c2c8676c38c484765339531ab05fee21f2e3
funkyFangs <funkyFangs@outlook.com>
2024-05-11 16:46:05 -0500

    :rotating_light: fix existing lint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,4 +26,15 @@ module.exports = {
       },
     },
   ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }
+    ]
+  },
 };
